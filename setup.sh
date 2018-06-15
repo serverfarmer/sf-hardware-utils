eda8d47bd998fefe6098257bb28336e02bebbf45
Tomasz Klim <github@tomaszklim.pl>
2018-06-15 19:33:56 +0200

    moved all functionality to new sf-storage-utils extension

--- setup.sh
+++ setup.sh
@@ -1,14 +1,3 @@
 #!/bin/sh
 
-
-# transitional code: rename standby.exceptions to skip-smart.devices
-if [ -f /etc/local/.config/standby.exceptions ] && [ ! -f /etc/local/.config/skip-smart.devices ]; then
-	mv /etc/local/.config/standby.exceptions /etc/local/.config/skip-smart.devices
-fi
-
-
-if [ ! -f /etc/local/.config/skip-smart.devices ]; then
-	echo "# devices to skip should be added in the below format:
-# /dev/disk/by-id/ata-ST4000DM000-1F2168_W300XXXX
-" >/etc/local/.config/skip-smart.devices
-fi
+/opt/farm/scripts/setup/extension.sh sf-storage-utils
